5f96fd2e73313c84f5dd1293b2cf375dadb87884
Mathias <mathipe@stud.ntnu.no>
2021-08-25 23:41:24 +0200

    Poop

--- Individual Assignment/decision_tree/decision_tree.py
+++ Individual Assignment/decision_tree/decision_tree.py
@@ -15,11 +15,12 @@ class Node:
                     and value is another Node-object
     """
 
-    def __init__(self, attribute):
-        # Attribute that corresponds to this node in the tree
-        self.attribute = attribute
-        # Node to keep track of branches. Key is v_k, value is a node (subtree)
-        self.branches = {}
+    def __init__(self):
+        # Attribute (i.e. Outlook, temp, etc.) that corresponds to this node in the tree
+        self.attribute = None
+        # Next node (if any) present in the tree
+        self.next = None
+        # 
 
     def add_branch(self, value, node):
         self.branches[value] = node
@@ -33,10 +34,54 @@ class DecisionTree:
     def __init__(self):
         # NOTE: Feel free add any hyperparameters 
         # (with defaults) as you see fit
-        x = 1
+        # Variable to keep track of the attribute we want to predict
+        self.goal = None
+        # Keep track of the global entropy for the entire set (to use in info gain formula)
+        self.total_entropy = None
 
+    def calculate_remainder(self, attribute, examples):
+        # Keep a running track of the result for summation
+        result = 0
+        # Iterate through every possible state for the attribute, and get p_k and n_k
+        for k in self.training_set[attribute].unique():
+            # Due to implementation, the last element is often NaN. So we just skip this
+            if pd.isnull(k):
+                continue
+            # Default values are 0 in case examples don't exist
+            p_k = 0
+            n_k = 0
+            # Count positive and negative outcomes
+            resulting_example_count = examples[examples[attribute] == k].groupby(self.goal)[attribute].count()
+            # Iterate through indexes in case examples for this don't exist
+            for i in resulting_example_count.index:
+                if i == 0:
+                    n_k = resulting_example_count[0]
+                if i == 1:
+                    p_k = resulting_example_count[1]
+            # If there are no negative nor positive examples, then we will add 0 anyway, so we just skip
+            if p_k == 0 and n_k == 0:
+                continue
+            # Else add the values in accordance with the summation formula
+            r1 = (p_k + n_k) / (self.p + self.n)
+            r2 = self.calculate_boolean_entropy((p_k / (p_k + n_k)))
+            result += r1 * r2
+        return result
 
-    def fit(self, X, y):
+    def calculate_boolean_entropy(self, q):
+        if q == 0 or q == 1:
+            # return -(0 + (1-0) log(1)) = 0
+            return 0
+        # return B(q)
+        r1 = q * np.log2(q, 2)
+        r2 = (1 - q) * np.log2(1 - q, 2)
+        return -(r1 + r2)
+
+    def get_plurality_value(self, examples):
+        # Examples will be a DataFrame with set attributes, for example ('Outlook' == 'Sunny') & ('Wind' == 'Weak')
+        # We therefore just access the goal-column of these combined attributes, and count how many there are
+        return 1 if examples[self.goal].value_counts()[1] > examples[self.goal].value_counts()[0] else 0
+
+    def fit(self, X, y, attributes=None):
         """
         Generates a decision tree for classification
         
@@ -45,17 +90,18 @@ class DecisionTree:
                 each row is a sample and the columns correspond
                 to the features.
             y (pd.Series): a vector of discrete ground-truth labels
+            attributes: list of attributes from the dataset
         """
         # TODO: Implement
         # First we calculate the entropy of the entire set by value_counts, which is an array containing the number of
         # positive and negative classifications for the target attribute (which in case 1 is 'Play Tennis')
-        set_entropy = entropy(y.value_counts())
-        print(set_entropy)
-        root_node = {}
-        target_attribute = y.name
+        self.total_entropy = entropy(y.value_counts())
+        # We then set the attribute we want to predict (goal)
+        self.goal = y.name
         # If there are only positive ('Yes') or negative ('No') attributes left, then return the current classification
-        if y.nunique() == 1:
-            return y[target_attribute]
+        classifications = X[self.goal].value_counts()
+        if len(classifications) == 1:
+            return classifications.index[0]
 
 
     def predict(self, X):
@@ -86,7 +132,7 @@ class DecisionTree:
             attr1=val1 ^ attr2=val2 ^ ... => label
         
         Example output:
-        >>> model.get_rules()
+        model.get_rules()
         [
             ([('Outlook', 'Overcast')], 'Yes'),
             ([('Outlook', 'Rain'), ('Wind', 'Strong')], 'No'),
@@ -137,7 +183,7 @@ def entropy(counts):
     return - np.sum(probs * np.log2(probs))
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     test_model = DecisionTree()
     train_set = pd.read_csv('data_1.csv')
     X = train_set.drop(columns=['Play Tennis'])
